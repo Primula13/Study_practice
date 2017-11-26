ef891f087a2bf414138b9ea5520b220ceb46bcfc
Primula13 <kornutaa@gmail.com>
2017-11-26 21:46:05 +0200

    Delete Random_FindMax_task.py

--- Random_FindMax_task.py
+++
@@ -1,9 +0,0 @@
-"""Сгенерировать массив из задачи b. Найти 2 максимальных элемента данного массива. Элементы не должны быть равны."""
-from random import randint
-
-
-def find_max_from_random():
-    random_list = [randint(-1000, 1000) for _ in range(1000)]
-    remove_doubles = set(random_list)
-    find_max = sorted(list(remove_doubles), reverse=True)
-    print(find_max[0:2])
